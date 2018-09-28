f66eddc066c5a51401a8be8acf12ba8e1767b3da
thedavidhk <david-herok@hotmail.de>
2018-09-28 12:43:10 +0200

    Setting up Project

---
+++ trunk/admin/partials/academic-publications-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://david-herok.com
+ * @since      1.0.0
+ *
+ * @package    Academic_Publications
+ * @subpackage Academic_Publications/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ trunk/includes/class-academic-publications-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://david-herok.com
+ * @since      1.0.0
+ *
+ * @package    Academic_Publications
+ * @subpackage Academic_Publications/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Academic_Publications
+ * @subpackage Academic_Publications/includes
+ * @author     David Herok <herok.davidj@gmail.com>
+ */
+class Academic_Publications_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
